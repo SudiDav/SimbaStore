cf782d078f1f0efb1ca5bddbdc7d65dd6e6d4000
Sudi Simba <sudisimbadav@gmail.com>
2022-01-19 00:18:37 +0200

    modified the api call by using createAdapter

--- client/src/features/Catalog/catalogSlice.ts
+++ client/src/features/Catalog/catalogSlice.ts
@@ -1,3 +1,4 @@
+import {RootState} from "./../../app/store/configureStore";
 import {
   createAsyncThunk,
   createEntityAdapter,
@@ -19,6 +20,17 @@ export const fetchProductsAsync = createAsyncThunk<Product[]>(
   }
 );
 
+export const fetchProductAsync = createAsyncThunk<Product, number>(
+  "catalog/fetchProductAsync",
+  async productId => {
+    try {
+      return await agent.Catalog.details(productId);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 export const catalogSlice = createSlice({
   name: "catalog",
   initialState: productsAdapter.getInitialState({
@@ -27,7 +39,7 @@ export const catalogSlice = createSlice({
   }),
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(fetchProductsAsync.pending, (state, action) => {
+    builder.addCase(fetchProductsAsync.pending, state => {
       state.status = "penddingFetchProducts";
     });
     builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
@@ -38,5 +50,19 @@ export const catalogSlice = createSlice({
     builder.addCase(fetchProductsAsync.rejected, state => {
       state.status = "idle";
     });
+    builder.addCase(fetchProductAsync.pending, state => {
+      state.status = "pendingFetchProduct";
+    });
+    builder.addCase(fetchProductAsync.fulfilled, (state, action) => {
+      productsAdapter.upsertOne(state, action.payload);
+      state.status = "idle";
+    });
+    builder.addCase(fetchProductAsync.rejected, state => {
+      state.status = "idle";
+    });
   },
 });
+
+export const productSelectors = productsAdapter.getSelectors(
+  (state: RootState) => state.catalog
+);
